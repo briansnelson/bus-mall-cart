2092d93d4511e94fd4fd9bbaf438688df7d05ab3
briansnelson <brian@grit360.com>
2021-06-08 21:35:34 -0700

    did two TODOs

--- js/catalog.js
+++ js/catalog.js
@@ -40,7 +40,11 @@ function addSelectedItemToCart() {
   // DONE: suss out the item picked from the select list
   let itemPicked = document.getElementById('items').value;
   console.log(itemPicked);
-  // TODO: get the quantity
+  // DONE: get the quantity
+  let itemQuantity = document.getElementById('quantity').value;
+  console.log(itemQuantity);
+  
+
   // TODO: using those, add one item to the Cart
 }
 
